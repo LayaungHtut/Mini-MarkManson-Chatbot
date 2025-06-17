62f1198196302dad99980f177c4014be356bf4f0
LaYaungHtut <layaunghtut5@gmail.com>
2025-06-17 21:43:44 +0630

    DaisyUi library components added

---
+++ src/lib/component/library/daisyui/DaisyUiCollapse.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { collapseTitle, collapseContent, className } = $props<{
+		collapseTitle: string;
+		collapseContent: string;
+		className?: string;
+	}>();
+</script>
+
+<div class={className}>
+	<input type="checkbox" />
+	<div class="d-collapse-title font-semibold">{collapseTitle}</div>
+	<div class="d-collapse-content text-sm">
+		{collapseContent}
+	</div>
+</div>
